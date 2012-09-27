cb3e98b2cce34b38fee5e3b077f5f932aea50c4b
Dmitriy Shabanov <shabanovd@gmail.com>
2012-09-27 15:33:51 +0500

    webcam panel

---
+++ src/main/java/org/animotron/animi/gui/Application.java
@@ -0,0 +1,104 @@
+/*
+ *  Copyright (C) 2012 The Animo Project
+ *  http://animotron.org
+ *
+ *  This file is part of Animotron.
+ *
+ *  Animotron is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  Animotron is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of
+ *  the GNU Affero General Public License along with Animotron.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.animotron.animi.gui;
+
+import java.awt.Button;
+import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+public class Application extends JFrame implements Runnable {
+	
+	private static final long serialVersionUID = 3243253015790558286L;
+
+	private WebcamPanel camView = null;
+	
+	private Application() {}
+	
+	@Override
+	public void run() {
+		setTitle("Animi");
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+        	@Override
+        	public void windowClosed(WindowEvent e) {
+        		stop();
+        	}
+
+        	@Override
+        	public void windowDeiconified(WindowEvent e) {
+        		resume();
+        	}
+
+			@Override
+        	public void windowIconified(WindowEvent e) {
+        		pause();
+        	}
+        });
+
+        setLayout(new GridLayout(2,2,2,2));
+        
+        Button bt = new Button("initialize");
+        add(bt);
+
+        bt = new Button("pause");
+        add(bt);
+
+        bt = new Button("continue");
+        add(bt);
+
+        camView = new WebcamPanel();
+        add(camView);
+        
+        setBounds(0, 0, 800, 600);
+
+		pack();
+		setVisible(true);
+
+	}
+
+	private void resume() {
+		if (camView != null)
+			camView.resume();
+	}
+
+	private void pause() {
+		if (camView != null)
+			camView.pause();
+	}
+
+	private void stop() {
+		if (camView != null)
+			camView.stop();
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Application());
+	}
+}
---
+++ src/main/java/org/animotron/animi/gui/WebcamPanel.java
@@ -0,0 +1,165 @@
+package org.animotron.animi.gui;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamEvent;
+import com.github.sarxos.webcam.WebcamListener;
+import com.github.sarxos.webcam.ds.openimaj.OpenImajDriver;
+
+/**
+ * Simply implementation of JPanel allowing users to render pictures taken with
+ * webcam.
+ * 
+ * @author Bartosz Firyn (SarXos)
+ */
+public class WebcamPanel extends JPanel implements WebcamListener {
+
+	private static final long serialVersionUID = 5792962512394656227L;
+
+	private int frequency = 65; // Hz
+
+	private class Repainter extends Thread {
+
+		public Repainter() {
+			setDaemon(true);
+		}
+
+		@Override
+		public void run() {
+			super.run();
+
+			while (webcam.isOpen()) {
+
+				image = webcam.getImage();
+
+				try {
+					if (paused) {
+						synchronized (this) {
+							this.wait();
+						}
+					}
+					Thread.sleep(1000 / frequency);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				WebcamPanel.this.repaint();
+			}
+		}
+	}
+
+	private Webcam webcam = null;
+	private BufferedImage image = null;
+	private Repainter repainter = null;
+
+	public WebcamPanel() {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+
+				Webcam.setDriver(new OpenImajDriver());
+				webcam = Webcam.getDefault();
+				if (webcam == null) {
+					System.out.println("No webcams found...");
+					System.exit(1);
+				}
+				webcam.addWebcamListener(WebcamPanel.this);
+
+				if (!webcam.isOpen()) {
+					webcam.open();
+				}
+			}
+		});
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+
+		super.paintComponent(g);
+
+		if (image == null) {
+			return;
+		}
+
+		g.drawImage(image, 0, 0, null);
+	}
+
+	@Override
+	public void webcamOpen(WebcamEvent we) {
+		if (repainter == null) {
+			repainter = new Repainter();
+		}
+		repainter.start();
+		setPreferredSize(webcam.getViewSize());
+	}
+
+	@Override
+	public void webcamClosed(WebcamEvent we) {
+		try {
+			repainter.join();
+			repainter = null;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private volatile boolean paused = false;
+
+	/**
+	 * Pause rendering.
+	 */
+	public void pause() {
+		if (paused) {
+			return;
+		}
+		paused = true;
+		System.out.println("paused");
+	}
+
+	/**
+	 * Resume rendering.
+	 */
+	public void resume() {
+		if (!paused) {
+			return;
+		}
+		synchronized (repainter) {
+			repainter.notifyAll();
+		}
+		paused = false;
+		System.out.println("resumed");
+	}
+	
+	public void stop() {
+		webcam.close();
+	}
+
+	/**
+	 * @return Rendering frequency (in Hz or FPS).
+	 */
+	public int getFrequency() {
+		return frequency;
+	}
+
+	/**
+	 * Set rendering frequency (in Hz or FPS). Min is 1 and max is 100.
+	 * 
+	 * @param frequency
+	 */
+	public void setFrequency(int frequency) {
+		if (frequency > 100) {
+			frequency = 100;
+		}
+		if (frequency < 1) {
+			frequency = 1;
+		}
+		this.frequency = frequency;
+	}
+
+}
