2b49289ccecd0bf769dad0b62ad1345f5db7ef0c
gev <gazdovsky@gmail.com>
2012-09-28 18:47:41 +0400

    visualisation

--- src/main/java/org/animotron/animi/gui/WebcamPanel.java
+++ src/main/java/org/animotron/animi/gui/WebcamPanel.java
@@ -100,11 +100,11 @@ public class WebcamPanel extends JPanel implements WebcamListener {
 		}
 
         g.drawImage(image, 0, 0, null);
-        g.drawImage(gray, 640, 0, null);
+        g.drawImage(gray, 643, 0, null);
         int x = 0;
         for (MultiCortex.SCortexZone zone : cortexs.zones) {
-            g.drawImage(zone.getColImage(), x, 480, null);
-            x += zone.getWidth();
+            g.drawImage(zone.getColImage(), x, 483, null);
+            x += zone.getWidth() + 3;
         }
     }
 
